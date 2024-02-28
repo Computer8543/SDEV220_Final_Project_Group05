800c5efbb15249a4afa0a3873eee99dd6d1d7024
Computer8543 <college1b7e@gmail.com>
2024-02-28 07:51:29 -0500

    Shifted the buttons over to the calculation class, shifted expression and index over to gui class, and made my first button (the equals button). Now I need to get the equals button to actually work, and add more buttons.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import tkinter as tk
 from tkinter import ttk
 
-    
 class gui(tk.Tk):
     """ Meant to act as the GUI of the calculator.
     """
-    def __init__(self):
+    def __init__(self, expression, index):
         super().__init__()
+        # expression is used to hold the whole line of data
+        self.expression: list[str] = expression
+        
+        # index is used to hold the index of the data
+        self.index: int = index
+        
         # Title of window
         self.title("Mario's Italian Restaurant Calculator")
         # Pixel size of window
@@ -14,7 +19,45 @@ class gui(tk.Tk):
         # To prevent you from resizing the window
         self.resizable(0,0) 
         
-        # initialize input_text variable
+     
+       
+
+class memory(gui):
+    """ Meant to store and validate data inputted into the calculator
+    """
+    def __init__(self, expression, index):
+        super().__init__(expression, index)
+        
+    def press_up(self, expression: list[str], index: int) -> list[str]:
+        """ When you press the up button, it goes back 1 in the index unless it is equal to or less than 0, where the index stops at 0 to prevent an out of bounds error
+
+        Args:
+            expression (list[str]): _description_
+            index (int): The index of the expression
+
+        Returns:
+            list[str]: The expression in the (index minus 1) unless the index is equal to or less than 0, else it would be zero
+        """
+        return expression[((index - 1) if index > 0 else 0)]
+    
+    def press_down(self, expression: list[str], index: int) -> list[str]:
+        """When you press the down button, it goes forward 1 in the index unless it is equal to or greater than 9, where it will default to 9 to save space
+
+        Args:
+            expression (list[str]):  1 index is a whole line of numbers and symbols inputted into the calculator, up to 10 lines total.
+            index (int): The index of the expression
+
+        Returns:
+            list[str]: The expression in the (index plus 1) unless the index is equal to or greater than 9, else it defaults to 9 to save space in memory
+        """
+        return expression[((index + 1) if index < 9 else 9)]
+    
+class calculation(memory):
+    """ Meant to store various functions called by GUI to calculate.
+    """
+    def __init__(self, expression, index):
+        super().__init__(expression, index)
+        # initialize input_text variable as well as two math and storage classes within this class
         self.input_text: str = ""
         
         # Main input window frame
@@ -30,58 +73,28 @@ class gui(tk.Tk):
         self.buttons_frame = tk.Frame(self, width=640, height=540, bd=0, highlightbackground="black", highlightcolor="black", highlightthickness=2)
         self.buttons_frame.pack(side="bottom")
         
-        # create buttons
-    
-class memory(gui):
-    """ Meant to store and validate data inputted into the calculator
-    """
-    def __init__(self):
-        super().__init__()
-        # expression is used to hold the whole line of data
-        self.expression: list[str] = [""]
-        # index is used to hold the index of the data
-        self.index: int = 0
-    def press_up(self, expression: list[str], index: int) -> list[str]:
-        """ When you press the up button, it goes back 1 in the index unless it is equal to or less than 0, where the index stops at 0 to prevent an out of bounds error
-
-        Args:
-            expression (list[str]): _description_
-            index (int): The index of the expression
-
-        Returns:
-            list[str]: The expression in the (index minus 1) unless the index is equal to or less than 0, else it would be zero
-        """
-        return expression[((index - 1) if index > 0 else 0)]
-    def press_down(self, expression: list[str], index: int) -> list[str]:
-        """When you press the down button, it goes forward 1 in the index unless it is equal to or greater than 9, where it will default to 9 to save space
-
-        Args:
-            expression (list[str]):  1 index is a whole line of numbers and symbols inputted into the calculator, up to 10 lines total.
-            index (int): The index of the expression
-
-        Returns:
-            list[str]: The expression in the (index plus 1) unless the index is equal to or greater than 9, else it defaults to 9 to save space in memory
-        """
-        return expression[((index + 1) if index < 9 else 9)]
+        # create first row of buttons
+        self.equal_button = tk.Button(self.buttons_frame, text = "=", foreground= "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command=self.button_equal(self.expression, self.index)).grid(row = 0, column = 5, padx = 1, pady = 1)
+        
     
     
-class calculation(memory):
-    """ Meant to store various functions called by GUI to calculate.
-    """
-    def __init__(self):
-        super().__init__()
     def button_equal(self, expression: list[str], index: int) -> None:
-        """ We find what number the expression is equal to and add 1 to the index
+        """ We find what number the expression is equal to and add 1 to the index if it doesn't create an error, else make an error message
 
         Args:
             expression (list[str]): 1 index is a whole line of numbers and symbols inputted into the calculator, up to 10 lines total.
             result (str): The end result of the mathematical expression
             index (int): The index of the expression
         """
-        result = str(eval(expression[index]))
-        calculation.input_text.set(result)
-        index += 1
-        return
+        try:
+            result = str(eval(expression[index]))
+            self.input_text = result
+            index += 1
+            return
+        except:
+            self.input_text = " error "
+            expression[index] = ""
+            
     def button_click(self, expression: list[str], index: int, input: str) -> None:
         """ We add the input to the expression through string concatenation
 
@@ -91,8 +104,9 @@ class calculation(memory):
             index (int): The index of the expression
         """
         expression[index] = expression[index] + str(input)
-        calculation.input_text.set(expression[index])
+        self.input_text = expression[index]
         return 
+    
     def button_clear(self, expression: list[str], index: int) -> None:
         """ We clear the input of the expression through creating an empty string as the input
 
@@ -101,10 +115,10 @@ class calculation(memory):
             index (int): The index of the expression
         """
         expression[index] = ""
-        calculation.input_text.set("")
-        return 
-
+        self.input_text = ""
+        return  
+      
 # start calculation class
-begin = calculation()
+begin = calculation([""], 0)
 
 begin.mainloop()
