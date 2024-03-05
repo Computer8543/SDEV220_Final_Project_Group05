2ff784a5e79bd3119777c39560b349016dccda49
Computer8543 <college1b7e@gmail.com>
2024-03-05 12:38:52 -0500

    Finally fixed the issue with the buttons not working by adding a lambda function in the command variable in the buttons. The number buttons work, but not the equals sign button, so I'll have to fix that later.

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import tkinter as tk
 
 
 # expression is used to hold the whole line of data
-expression: list[str] = [""]
+expression: list[str] = ["", "", "", "", "", "", "", "", "", ""]
         
 # index is used to hold the index of the data
 index: int = 0
 
-# equation is the name of the StringVar() object, and thus will display on the input box of the calculator
-equation: str = ""
+
 
 class gui(tk.Tk):
     """ Meant to act as the GUI of the calculator.
@@ -23,18 +22,16 @@ class gui(tk.Tk):
         # To prevent you from resizing the window
         self.resizable(0,0) 
         
-        
-        
         # Main input window frame
         self.input_window = tk.Frame(self, width=480, height=100, bd=0, highlightbackground="black", highlightcolor="black", highlightthickness=2)
         self.input_window.pack(side="top")
         
-        # initialize input_text variable 
-        self.input_text: str = tk.StringVar(self.input_window, "", equation)
+        # equation is the name of the StringVar() object, and thus will display on the input box of the calculator
+        self.equation = tk.StringVar()
         
         
         # Input field where the expression text goes
-        self.input_field = tk.Label(self.input_window, font=('times new roman', 20, 'bold'), text=self.input_text, width=50, bg="#eee", bd=2, justify='right')
+        self.input_field = tk.Entry(self.input_window, font=('times new roman', 20, 'bold'), textvariable=self.equation, width=50, bg="#eee", bd=2, justify='right')
         self.input_field.grid(row=0, column=0)
         self.input_field.pack(ipady=10)
         self.input_field.focus_set()
@@ -44,10 +41,13 @@ class gui(tk.Tk):
         self.buttons_frame = tk.Frame(self, width=640, height=540, bd=0, highlightbackground="black", highlightcolor="black", highlightthickness=2)
         self.buttons_frame.pack()
         
-        self.equal_button = tk.Button(self.buttons_frame, text = "=", foreground= "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command=self.button_equal()).grid(row = 0, column = 5, padx = 1, pady = 1)
-        self.nine_button = tk.Button(self.buttons_frame, text = "9", foreground= "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command=self.button_click(9)).grid(row = 0, column = 4, padx = 1, pady = 1)
+        # first row of buttons
+        self.equal_button = tk.Button(self.buttons_frame, text = "=", foreground= "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command= lambda: self.button_equal()).grid(row = 0, column = 5, padx = 1, pady = 1)
+        self.nine_button = tk.Button(self.buttons_frame, text = "9", foreground= "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command= lambda: self.button_click(9)).grid(row = 0, column = 4, padx = 1, pady = 1)
+        self.eight_button = tk.Button(self.buttons_frame, text = "8", foreground= "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command= lambda: self.button_click(8)).grid(row = 0, column = 3, padx = 1, pady = 1)
         
-        
+        # begin mainloop
+        self.mainloop()
         
     def press_up(self) -> list[str]:
         """ When you press the up button, it goes back 1 in the index unless it is equal to or less than 0, where the index stops at 0 to prevent an out of bounds error
@@ -78,9 +78,11 @@ class gui(tk.Tk):
         return expression[((index + 1) if index < 9 else 9)]
     
     
-    def button_equal(self) -> None:
-        """ We find what number the expression is equal to and add 1 to the index if it doesn't create an error, else make an error message
-
+    def button_equal(self) -> str:
+        """ We find what number the expression is equal to 
+        and add 1 to the index unless it is 9, 
+        where we set the index back to 0. If it creates an error, 
+        make an error message.
         Args:
             expression (list[str]): 1 index is a whole line of numbers and symbols inputted into the calculator, up to 10 lines total.
             result (str): The end result of the mathematical expression
@@ -88,16 +90,20 @@ class gui(tk.Tk):
         """
         global expression
         global index
-        global equation
+        
         
         try:
             result = str(eval(expression[index]))
-            equation = result
-            return
+            if index < 9:
+                index += 1
+            else:
+                index = 0
+            return self.equation.set(result)
         except:
-            self.input_text = " error "
+            self.equation.set(" error ")
             expression[index] = ""
             return
+    
         
     def button_click(self, input) -> str:
         """ We add the input to the expression through string concatenation
@@ -109,11 +115,9 @@ class gui(tk.Tk):
         """
         global expression
         global index
-        global equation
         
         expression[index] = expression[index] + str(input)
-        equation = expression[index]
-        return equation
+        return self.equation.set(expression[index]) 
     
     def button_clear(self) -> None:
         """ We clear the input of the expression through creating an empty string as the input
@@ -126,14 +130,13 @@ class gui(tk.Tk):
         global index
         
         expression[index] = ""
-        self.input_text = ""
+        self.equation.set("")
         return
       
       
      
        
 
-# start calculation class
+# initalize gui class
 begin = gui()
 
-begin.mainloop()
